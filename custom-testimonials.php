df3096e695f10335e2e3f214c6f2ee5797e31ec8
kidcompassion <sallypoulsen@gmail.com>
2020-02-13 14:22:31 -0700

    Saves updates

--- custom-testimonials.php
+++ custom-testimonials.php
@@ -13,7 +13,7 @@
 *	Sets up the testimonial custom post type
 */
 
-function x_create_testimonials() {
+function x_testimonials_create_testimonials() {
  
     register_post_type( 'testimonials',
         array(
@@ -39,23 +39,23 @@ function x_create_testimonials() {
     );
 }
 
-add_action( 'init', 'x_create_testimonials' );
+add_action( 'init', 'x_testimonials_create_testimonials' );
 
 
-function x_testimonials_stylesheet() {
+function x_testimonials_testimonials_stylesheet() {
     wp_enqueue_style( 'testimonial-styles', plugins_url('testimonial_styles.css', __FILE__) );
     wp_enqueue_script( 'testimonial-js', plugins_url('testimonials.js', __FILE__), array('jquery') );
     wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css');
 }
 
-add_action( 'wp_enqueue_scripts', 'x_testimonials_stylesheet' );
+add_action( 'wp_enqueue_scripts', 'x_testimonials_testimonials_stylesheet' );
 
 
 /*
     Generates single template for testimonials
 */
 
-function x_custom_single_template( $template ) {
+function x_testimonials_custom_single_template( $template ) {
     global $post;
 
     if ( 'testimonials' === $post->post_type && locate_template( array( 'templates/testimonial.php' ) ) !== $template ) {
@@ -64,13 +64,13 @@ function x_custom_single_template( $template ) {
     return $template;
 }
 
-add_filter( 'single_template', 'x_custom_single_template' );
+add_filter( 'single_template', 'x_testimonials_custom_single_template' );
 
 /*
     Generates archive template for testimonials
 */
 
-function x_custom_archive_template( $template ) {
+function x_testimonials_custom_archive_template( $template ) {
     global $post;
     if ( is_post_type_archive ( 'testimonials' ) ) {
         if ( 'testimonials' === $post->post_type && locate_template( array( 'templates/testimonials-index.php' ) ) !== $template ) {
@@ -79,9 +79,9 @@ function x_custom_archive_template( $template ) {
     }
     return $template;
 }
-add_filter( 'archive_template', 'x_custom_archive_template' );
+add_filter( 'archive_template', 'x_testimonials_custom_archive_template' );
 
-function x_testimonial_slider_shortcode( $atts ){
+function x_testimonials_testimonial_slider_shortcode( $atts ){
     // Uses output buffering so I can avoid manually concatenating the template.
     // WP shortcodes require a value be passed via "return" - if it isn't, it will, by default, load at the top of the page
     ob_start();
@@ -89,22 +89,22 @@ function x_testimonial_slider_shortcode( $atts ){
     $ob = ob_get_clean();
     return $ob;
 }
-add_shortcode( 'testimonials_slider', 'x_testimonial_slider_shortcode' );
+add_shortcode( 'testimonials_slider', 'x_testimonials_testimonial_slider_shortcode' );
 
 /**
  * Register meta boxes.
  */
-function x_register_meta_boxes() {
-    add_meta_box( 'x-1', __( 'Testimonial Attribution', 'x' ), 'x_display_callback', 'testimonials', 'normal', 'high' );
+function x_testimonials_register_meta_boxes() {
+    add_meta_box( 'x-1', __( 'Testimonial Attribution', 'x' ), 'x_testimonials_display_callback', 'testimonials', 'normal', 'high' );
 }
-add_action( 'add_meta_boxes', 'x_register_meta_boxes' );
+add_action( 'add_meta_boxes', 'x_testimonials_register_meta_boxes' );
 
 /**
  * Meta box display callback.
  *
  * @param WP_Post $post Current post object.
  */
-function x_display_callback( $post ) {
+function x_testimonials_display_callback( $post ) {
     include plugin_dir_path( __FILE__ ) . './templates/form.php';
 
 }
@@ -115,14 +115,14 @@ function x_display_callback( $post ) {
  *
  * @param int $post_id Post ID
  */
-function x_save_meta_box( $post_id ) {
+function x_testimonials_save_meta_box( $post_id ) {
     if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
     if ( $parent_id = wp_is_post_revision( $post_id ) ) {
         $post_id = $parent_id;
     }
     $fields = [
-        'x_attr_name',
-        'x_attr_title',
+        'x_testimonials_attr_name',
+        'x_testimonials_attr_title',
     ];
     foreach ( $fields as $field ) {
         if ( array_key_exists( $field, $_POST ) ) {
@@ -130,7 +130,7 @@ function x_save_meta_box( $post_id ) {
         }
      }
 }
-add_action( 'save_post', 'x_save_meta_box' );
+add_action( 'save_post', 'x_testimonials_save_meta_box' );
 
 
 
